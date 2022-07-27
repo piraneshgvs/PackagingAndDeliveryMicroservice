e13fde8166aea2cf93c17a580ce7d0c201d76c76
spran <spran@Piranesh>
2022-07-27 10:01:34 +0530

    Added the exception handling

---
+++ PackagingAndDeliveryMicroservice/src/main/java/com/cognizant/packaging/delivery/constant/GeneralConstant.java
@@ -0,0 +1,7 @@
+package com.cognizant.packaging.delivery.constant;
+
+public class GeneralConstant {
+	
+	public static String EXCEPTION_MESSAGE = "Component type or Count of the component is null or zero";
+
+}
--- PackagingAndDeliveryMicroservice/src/main/java/com/cognizant/packaging/delivery/controller/PackagingController.java
+++ PackagingAndDeliveryMicroservice/src/main/java/com/cognizant/packaging/delivery/controller/PackagingController.java
@@ -1,10 +1,13 @@
 package com.cognizant.packaging.delivery.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cognizant.packaging.delivery.constant.GeneralConstant;
 import com.cognizant.packaging.delivery.service.CalculatingService;
 
 
@@ -15,11 +18,14 @@ public class PackagingController {
 	CalculatingService calculatingService;
 	
 	@GetMapping("/GetPackagingDeliveryCharge")
-	public int packageDelivery(@RequestParam String componentType, int count) {
-		
-	    int finalCost = calculatingService.calculateCost(componentType, count);
+	public ResponseEntity<?> packageDelivery(@RequestParam String componentType, int count) {
+		int finalCost=0;
+		if(componentType!=null&&count>0) {
+			  finalCost = calculatingService.calculateCost(componentType, count);
+			  return new ResponseEntity<Integer>(finalCost,HttpStatus.OK);
+		}
 	    
-		return finalCost;
+		return new ResponseEntity<>(GeneralConstant.EXCEPTION_MESSAGE, HttpStatus.BAD_REQUEST);
 	}
 
 }
