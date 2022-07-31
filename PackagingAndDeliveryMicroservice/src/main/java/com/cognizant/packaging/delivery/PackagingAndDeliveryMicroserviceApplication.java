11d8a19ee928cad1570514bc7595bd9373f376e5
spran <spran@Piranesh>
2022-07-31 15:28:55 +0530

    Added feign client

--- PackagingAndDeliveryMicroservice/src/main/java/com/cognizant/packaging/delivery/PackagingAndDeliveryMicroserviceApplication.java
+++ PackagingAndDeliveryMicroservice/src/main/java/com/cognizant/packaging/delivery/PackagingAndDeliveryMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.cognizant.packaging.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PackagingAndDeliveryMicroserviceApplication {
 
 	public static void main(String[] args) {
